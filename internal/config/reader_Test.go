b40a2782f9b1b9923656efa47efdfdfc6b36c3e7
AmirAbbas <amirabas.khoshbayan@gmail.com>
2021-05-12 01:40:09 +0430

    add reader.go to folder internal/config/ for test and validate

---
+++ internal/config/reader_Test.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	type args struct {
+		path string
+		cfg  *Config
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "parse config.yaml",
+			args: args{
+				path: "testdata/config.yaml",
+				cfg:  &Config{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "parse config.yml",
+			args: args{
+				path: "testdata/config.yml",
+				cfg:  &Config{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "error unknown file extension",
+			args: args{
+				path: "testdata/config.conf",
+				cfg:  &Config{},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Parse(tt.args.path, tt.args.cfg); (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
